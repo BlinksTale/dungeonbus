e427cef1f34a57959736a5b9512192102857b599
Brian Handy <BlinksTale@gmail.com>
2015-01-24 10:04:32 -0800

    Wheel based driving implemented

--- trunk/Assets/Scripts/WheelController.cs
+++ trunk/Assets/Scripts/WheelController.cs
@@ -3,7 +3,15 @@ using System.Collections;
 
 public class WheelController : MonoBehaviour {
 
-	WheelCollider wheel;
+	public bool steering = true;
+	public bool invertDirection = false;
+	public float speed = 20f;
+
+	private WheelCollider wheel;
+	private float turningSpeed = 3f; // speed of 2f has some good chunkiness
+	private float steerAngleMax = 45f;
+
+	private bool turnRight, turnLeft;
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +21,25 @@ public class WheelController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			wheel.motorTorque = 1000f;
+			wheel.motorTorque = 50f*speed;
 		} else {
-			wheel.motorTorque = 10f;
+			wheel.motorTorque = 1f*speed;
 		}
-			
+
+		if (steering) {
+			turnLeft = 	(Input.GetKey (KeyCode.LeftArrow)  && !invertDirection) || 
+						(Input.GetKey (KeyCode.RightArrow) && invertDirection);
+			turnRight = (Input.GetKey (KeyCode.LeftArrow)  && invertDirection) || 
+						(Input.GetKey (KeyCode.RightArrow) && !invertDirection);
+
+			if (turnLeft && wheel.steerAngle > -steerAngleMax) {
+				wheel.steerAngle -= turningSpeed;
+			}
+			if (turnRight && wheel.steerAngle < steerAngleMax) {
+				wheel.steerAngle += turningSpeed;
+			}
+		}
+
 //		rigidbody.AddForce(new Vector3(0f, 0f, 1000f));
 //		rigidbody.AddTorque(new Vector3(1000f, 1000f, 1000f));
 	}
