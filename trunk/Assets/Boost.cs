f5e14e1b5bf169b5fffe2b6860a170b78547226d
Andrew Thompson <andrew@emberlab.com>
2015-01-25 14:36:24 -0800

    Added Boost to level 3

---
+++ trunk/Assets/Boost.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boost : MonoBehaviour
+{
+
+    private WheelController[] wheelCons;
+
+    public float boostSpeed = 2000f;
+    private float origSpeed;
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            wheelCons = col.gameObject.GetComponentsInChildren<WheelController>();
+
+            foreach (WheelController wc in wheelCons)
+            {
+               //col.rigidbody.AddRelativeForce(new Vector3(0, 100f, 0), ForceMode.Impulse);
+                origSpeed = wc.speed;
+                wc.speed += boostSpeed;
+            }
+
+            this.collider.enabled = false;
+            StartCoroutine(DisableBoost());
+            
+        }
+    }
+
+    IEnumerator DisableBoost()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        foreach (WheelController wc in wheelCons)
+        {
+            wc.speed = origSpeed;
+        }
+
+    }
+
+}
