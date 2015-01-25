8b478e4bd97e4294e767dac386f225c367f866a9
Brian Handy <BlinksTale@gmail.com>
2015-01-25 00:26:11 -0800

    Title screen added

---
+++ trunk/Assets/LoadLevelButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelButton : MonoBehaviour {
+
+	public string levelName = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void LoadLevel () {
+		Debug.Log ("Mouse press detected");
+		Application.LoadLevel(levelName);
+	}
+}
